4c18d88840c75c999db0ebf9a95c4279259a89ff
Lisa Lehrman <lisa.lehrman@gmail.com>
2016-02-20 12:09:25 -0800

    removed backend

--- public/js/app.js
+++ public/js/app.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
   // var $lastClickedId;
   // shuffle function
   $("#reset").on("click", function shuffle(event){
+    alert('afsadfasdffdsafasd');
       var parent = $("#shuffle");
       var divs = parent.children();
       while (divs.length) {
